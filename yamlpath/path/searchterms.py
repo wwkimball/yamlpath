81f4249ff44c607721a46a511d08542d736c31bc
William Kimball <30981667+wwkimball@users.noreply.github.com>
2019-05-21 19:43:37 -0500

    Eliminated all mypy and pylint noise

--- yamlpath/path/searchterms.py
+++ yamlpath/path/searchterms.py
@@ -1,4 +1,5 @@
-"""YAML path Search segment terms.
+"""
+YAML path Search segment terms.
 
 Copyright 2019 William W. Kimball, Jr. MBA MSIS
 """
@@ -10,10 +11,10 @@ class SearchTerms:
 
     def __init__(self, inverted: bool, method: PathSearchMethods,
                  attribute: str, term: str) -> None:
-        self.inverted = inverted
-        self.method = method
-        self.attribute = attribute
-        self.term = term
+        self._inverted: bool = inverted
+        self._method: PathSearchMethods = method
+        self._attribute: str = attribute
+        self._term: str = term
 
     def __str__(self) -> str:
         if self.method == PathSearchMethods.REGEX:
@@ -35,3 +36,36 @@ class SearchTerms:
             + safe_term
             + "]"
         )
+
+    @property
+    def inverted(self) -> bool:
+        """
+        Accesses the inversion flag for this Search, indicating whether the
+        results are to be inverted.
+        """
+        return self._inverted
+
+    @property
+    def method(self) -> PathSearchMethods:
+        """
+        Accesses the search method, indicating what kind of search is to be
+        performed.
+        """
+        return self._method
+
+    @property
+    def attribute(self) -> str:
+        """
+        Accessor for the attribute being searched.  This is the "haystack" and
+        may reference a particular dictionary key, all values of a dictionary,
+        or the elements of a list.
+        """
+        return self._attribute
+
+    @property
+    def term(self) -> str:
+        """
+        Accessor for the search term.  This is the "needle" to search for
+        within the attribute ("haystack").
+        """
+        return self._term
