6bae3102819327237b28c495d09712bd52ee4635
William Kimball <30981667+wwkimball@users.noreply.github.com>
2020-10-08 23:58:07 -0500

    Greatly enhanced ConsolePrinter::debug()

--- yamlpath/wrappers/consoleprinter.py
+++ yamlpath/wrappers/consoleprinter.py
@@ -14,7 +14,7 @@ Requires an object on init which has the following properties:
 Copyright 2018, 2019, 2020 William W. Kimball, Jr. MBA MSIS
 """
 import sys
-
+from typing import Any, Dict, Generator, List
 
 class ConsolePrinter:
     """
@@ -137,7 +137,7 @@ class ConsolePrinter:
         self.debug("Terminating with exit code, {}.".format(exit_code))
         sys.exit(exit_code)
 
-    def debug(self, message):
+    def debug(self, message, **kwargs):
         """
         Write a debug message to STDOUT unless quiet mode is active.
 
@@ -147,39 +147,143 @@ class ConsolePrinter:
         Positional Parameters:
         1. message (str) The message to print
 
+        Keyword Arguments:
+        * header
+        * footer
+        * prefix
+        * data
+
         Returns:  N/A
 
         Raises:  N/A
         """
         if self.args.debug and not self.args.quiet:
-            if isinstance(message, list):
-                for i, ele in enumerate(message):
-                    attr = ""
-                    if hasattr(ele, "anchor") and ele.anchor.value is not None:
-                        attr = "; &" + ele.anchor.value
-                    eattr = (str(ele) + attr).replace("\n", "\nDEBUG:  ")
-                    print("DEBUG:  [{}]={} {}".format(i, eattr, type(ele)))
-            elif isinstance(message, dict):
-                for key, val in message.items():
-                    key_anchor = (
-                        key.anchor.value if hasattr(key, "anchor") else None
-                    )
-                    val_anchor = (
-                        val.anchor.value if hasattr(val, "anchor") else None
-                    )
-                    line_out = "DEBUG:  [" + str(key)
-                    if key_anchor:
-                        line_out += "; &" + str(key_anchor)
-                    line_out += "]=>" + str(val).replace("\n", "\nDEBUG:  ")
-                    if val_anchor:
-                        line_out += "; &" + str(val_anchor)
-                    print(line_out)
-            else:
-                attr = ""
-                if (
-                        hasattr(message, "anchor")
-                        and message.anchor.value is not None
+            header = kwargs.pop("header", "")
+            footer = kwargs.pop("footer", "")
+            prefix = kwargs.pop("prefix", "")
+
+            if header:
+                print(ConsolePrinter._debug_prefix_lines(
+                    "{}{}".format(prefix, header)))
+
+            for line in ConsolePrinter._debug_dump(message, prefix=prefix):
+                print(line)
+
+            if "data" in kwargs:
+                data_header = kwargs.pop("data_header", "")
+                data_footer = kwargs.pop("data_footer", "")
+
+                if data_header:
+                    print(ConsolePrinter._debug_prefix_lines(
+                        "{}{}".format(prefix, data_header)))
+
+                for line in ConsolePrinter._debug_dump(
+                    kwargs.pop("data"), prefix=prefix, print_type=True
                 ):
-                    attr = "; &" + message.anchor.value
-                mattr = (str(message) + attr).replace("\n", "\nDEBUG:  ")
-                print("DEBUG:  " + str(mattr))
+                    print(line)
+
+                if data_footer:
+                    print(ConsolePrinter._debug_prefix_lines(
+                        "{}{}".format(prefix, data_footer)))
+
+            if footer:
+                print(ConsolePrinter._debug_prefix_lines(
+                    "{}{}".format(prefix, footer)))
+
+    @classmethod
+    def _debug_prefix_lines(cls, line: str) -> str:
+        return "DEBUG:  {}".format(str(line).replace("\n", "\nDEBUG:  "))
+
+    @classmethod
+    def _debug_get_anchor(cls, data: Any) -> str:
+        return ("&{}".format(data.anchor.value)
+                if hasattr(data, "anchor") and data.anchor.value is not None
+                else "")
+
+    @classmethod
+    def _debug_dump(cls, data: Any, **kwargs) -> Generator[str, None, None]:
+        """Helper for debug."""
+        prefix = kwargs.pop("prefix", "")
+        if isinstance(data, dict):
+            for line in ConsolePrinter._debug_dict(
+                data, prefix=prefix, **kwargs
+            ):
+                yield line
+        elif isinstance(data, list):
+            for line in ConsolePrinter._debug_list(
+                data, prefix=prefix, **kwargs
+            ):
+                yield line
+        else:
+            yield ConsolePrinter._debug_scalar(data, prefix=prefix, **kwargs)
+
+    @classmethod
+    def _debug_scalar(cls, data: Any, **kwargs) -> str:
+        prefix = kwargs.pop("prefix", "")
+        print_anchor = kwargs.pop("print_anchor", True)
+        print_type = kwargs.pop("print_type", False)
+
+        if print_anchor:
+            anchor = ConsolePrinter._debug_get_anchor(data)
+            if anchor:
+                prefix += "({})".format(anchor)
+
+        return ConsolePrinter._debug_prefix_lines(
+            "{}{}{}".format(prefix, data, (type(data) if print_type else "")))
+
+    @classmethod
+    def _debug_list(cls, data: List, **kwargs) -> Generator[str, None, None]:
+        """Helper for debug."""
+        prefix = kwargs.pop("prefix", "")
+        for idx, ele in enumerate(data):
+            ele_prefix = "{}[{}]".format(prefix, idx)
+            if isinstance(ele, dict):
+                for line in ConsolePrinter._debug_dict(ele, prefix=ele_prefix):
+                    yield line
+            elif isinstance(ele, list):
+                for line in ConsolePrinter._debug_list(ele, prefix=ele_prefix):
+                    yield line
+            else:
+                yield ConsolePrinter._debug_scalar(
+                    ele, prefix=ele_prefix, print_type=True)
+
+    @classmethod
+    def _debug_dict(cls, data: Dict, **kwargs) -> Generator[str, None, None]:
+        """Helper for debug."""
+        prefix = kwargs.pop("prefix", "")
+
+        local_keys = []
+        if (hasattr(data, "non_merged_items")
+            and callable(data.non_merged_items)
+        ):
+            for local_key, _ in data.non_merged_items():
+                local_keys.append(local_key)
+        else:
+            for key in data.keys():
+                local_keys.append(key)
+
+        for key, val in data.items():
+            display_key = (str(key)
+                           if key in local_keys
+                           else "<<:{}:>>".format(key))
+            key_anchor = ConsolePrinter._debug_get_anchor(key)
+            val_anchor = ConsolePrinter._debug_get_anchor(val)
+            display_anchor = ""
+            if key_anchor and val_anchor:
+                display_anchor = "({},{})".format(key_anchor, val_anchor)
+            elif key_anchor:
+                display_anchor = "({},_)".format(key_anchor)
+            elif val_anchor:
+                display_anchor = "(_,{})".format(val_anchor)
+
+            kv_prefix = "{}[{}]{}".format(prefix, display_key, display_anchor)
+
+            if isinstance(val, dict):
+                for line in ConsolePrinter._debug_dict(val, prefix=kv_prefix):
+                    yield "{}".format(line)
+            elif isinstance(val, list):
+                for line in ConsolePrinter._debug_list(val, prefix=kv_prefix):
+                    yield "{}".format(line)
+            else:
+                yield ConsolePrinter._debug_scalar(
+                    val, prefix=kv_prefix, print_type=True, print_anchor=False)
