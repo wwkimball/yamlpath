58e0af0b495f4f8dfec4866c1b725d02108dc660
William Kimball <30981667+wwkimball@users.noreply.github.com>
2020-10-28 21:23:00 -0500

    Update CHANGES and version for the work delta

--- yamlpath/__init__.py
+++ yamlpath/__init__.py
@@ -1,6 +1,6 @@
 """Core YAML Path classes."""
 # Establish the version number common to all components
-__version__ = "3.3.0"
+__version__ = "3.3.1"
 
 from yamlpath.yamlpath import YAMLPath
 from yamlpath.processor import Processor
