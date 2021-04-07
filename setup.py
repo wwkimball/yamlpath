c280ed6a0549d68389c6fb22b0d2a985663120da
William Kimball <30981667+wwkimball@users.noreply.github.com>
2021-04-07 15:04:21 -0500

    Excluding ruamel.yaml 0.17.x due to expected breaking changes per https://sourceforge.net/p/ruamel-yaml/tickets/351/#e9fc

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     },
     python_requires=">3.6.0",
     install_requires=[
-        "ruamel.yaml>=0.15.96",
+        "ruamel.yaml>=0.15.96,<0.17",
     ],
     tests_require=[
         "pytest",
