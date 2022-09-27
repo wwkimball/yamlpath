b0668ececb48f7680059cf6d4c00188575efbf07
William Kimball <30981667+wwkimball@users.noreply.github.com>
2022-09-27 13:47:45 -0500

    Version bump and change log update

--- yamlpath/__init__.py
+++ yamlpath/__init__.py
@@ -1,6 +1,6 @@
 """Core YAML Path classes."""
 # Establish the version number common to all components
-__version__ = "3.6.6"
+__version__ = "3.6.7"
 
 from yamlpath.yamlpath import YAMLPath
 from yamlpath.processor import Processor
