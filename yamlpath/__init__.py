428fc20d3bebc4f5a700f1d87f8e1d15a54c51e8
William Kimball <30981667+wwkimball@users.noreply.github.com>
2022-09-27 21:10:53 -0500

    Mark and credit compatibility with Python 3.10

--- yamlpath/__init__.py
+++ yamlpath/__init__.py
@@ -1,6 +1,6 @@
 """Core YAML Path classes."""
 # Establish the version number common to all components
-__version__ = "3.6.7"
+__version__ = "3.7.0"
 
 from yamlpath.yamlpath import YAMLPath
 from yamlpath.processor import Processor
