a0cbc0d996e150f3145554d07981986c11c01d76
William W. Kimball, Jr., MBA, MSIS <30981667+wwkimball@users.noreply.github.com>
2023-10-18 15:37:40 -0500

    Bugfix/220 (#222)
    * Fix #220
    * Mention #220 in CHANGES

--- yamlpath/__init__.py
+++ yamlpath/__init__.py
@@ -1,6 +1,6 @@
 """Core YAML Path classes."""
 # Establish the version number common to all components
-__version__ = "3.8.0"
+__version__ = "3.8.1"
 
 from yamlpath.yamlpath import YAMLPath
 from yamlpath.processor import Processor
