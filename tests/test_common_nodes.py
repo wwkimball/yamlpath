6e214ede01d0b28e062cb453f2df6aefacfadb21
William Kimball <30981667+wwkimball@users.noreply.github.com>
2026-04-09 19:26:31 -0500

    Address Pyright issues

--- tests/test_common_nodes.py
+++ tests/test_common_nodes.py
@@ -7,16 +7,8 @@ from ruamel.yaml.scalarstring import PlainScalarString
 from ruamel.yaml.scalarbool import ScalarBoolean
 from ruamel.yaml.scalarfloat import ScalarFloat
 from ruamel.yaml.scalarint import ScalarInt
-from ruamel.yaml import version_info as ryversion
-if ryversion < (0, 17, 22):                   # pragma: no cover
-    from yamlpath.patches.timestamp import (
-        AnchoredTimeStamp,
-        AnchoredDate,
-    )  # type: ignore
-else:                                         # pragma: no cover
-    # Temporarily fool MYPY into resolving the future-case imports
-    from ruamel.yaml.timestamp import TimeStamp as AnchoredTimeStamp
-    AnchoredDate = AnchoredTimeStamp
+
+from yamlpath.patches.timestamp import AnchoredTimeStamp, AnchoredDate
     #from ruamel.yaml.timestamp import AnchoredTimeStamp
     # From whence shall come AnchoredDate?
 
--- tests/test_common_parsers.py
+++ tests/test_common_parsers.py
@@ -3,16 +3,8 @@ import json
 import datetime as dt
 
 import ruamel.yaml as ry
-from ruamel.yaml import version_info as ryversion
-if ryversion < (0, 17, 22):                   # pragma: no cover
-    from yamlpath.patches.timestamp import (
-        AnchoredTimeStamp,
-        AnchoredDate,
-    )  # type: ignore
-else:                                         # pragma: no cover
-    # Temporarily fool MYPY into resolving the future-case imports
-    from ruamel.yaml.timestamp import TimeStamp as AnchoredTimeStamp
-    AnchoredDate = AnchoredTimeStamp
+
+from yamlpath.patches.timestamp import AnchoredTimeStamp, AnchoredDate
     #from ruamel.yaml.timestamp import AnchoredTimeStamp
     # From whence shall come AnchoredDate?
 
